da01e2f83acc43b35b2e9b15d9b5745037b21f1d
Ajand Mardalizad <aseedgeek@gmail.com>
2022-03-22 20:00:42 +0430

    project initiated

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      black: "#0B0420",
+      blue: "#2D36DE",
+      purple: "#1D0C46",
+      "light-purple": "#504366",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#E2E4F0",
+      "gray-light": "#d3dce6",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
