1b605a4006623192b663ffedd3c2138cd63a97e5
Ajand Mardalizad <aseedgeek@gmail.com>
2022-03-23 05:22:54 +0430

    Vault Table Added

--- src/modules/layouts/Desktop.js
+++ src/modules/layouts/Desktop.js
@@ -1,10 +1,16 @@
 import Sidebar from "../components/Sidebar";
+import VaultsTable from "../vault/VaultsTable";
 
 const Desktop = () => {
   return (
-    <div>
-      <div className="w-3/12">
+    <div className="flex">
+      <div className="w-3/12 inline-block">
         <Sidebar />
+      </div>
+      <div className="w-9/12 inline-block">
+        <div className="m-10">
+          <VaultsTable />
+        </div>
       </div>
     </div>
   );
---
+++ src/modules/vault/VaultsTable.js
@@ -0,0 +1,64 @@
+const VaultsTable = () => {
+  return (
+    <div class="relative overflow-x-auto shadow-md font-mono">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-violet-900 dark:text-gray-50">
+          <tr >
+            <th scope="col" class="px-6 py-3">
+              Strategist
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Vault Name
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Deposit Token
+            </th>
+            <th scope="col" class="px-6 py-3">
+              TVL
+            </th>
+            <th scope="col" class="px-6 py-3">
+              APR
+            </th>
+            <th scope="col" class="px-6 py-3">
+              My Deposits
+            </th>
+            <th scope="col" class="px-6 py-3 text-center">
+              Ape
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+            >
+              ajand.eth
+            </th>
+            <td class="px-6 py-4">wBTC</td>
+            <td class="px-6 py-4">wBTC</td>
+            <td class="px-6 py-4">$56,238</td>
+            <td class="px-6 py-4">1,560%</td>
+            <td class="px-6 py-4">$1283</td>
+            <td class="px-6 pt-3.5 flex justify-center items-end">
+              <button
+                type="button"
+                class="text-white text-center bg-blue-700 hover:bg-blue-800  text-xs px-2 py-1 mr-2 mb-2 dark:bg-blue-600  focus:outline-none "
+              >
+                Deposit
+              </button>
+              <button
+                type="button"
+                class="text-white text-center bg-blue-700 hover:bg-blue-800  text-xs px-2 py-1 mr-2 mb-2 dark:bg-blue-600  focus:outline-none "
+              >
+                Withdraw
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default VaultsTable;
